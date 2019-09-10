e723191f997f4297154a04218ae4c46cd8e89eaa
fsrd017 <277892799@qq.com>
2019-09-10 22:51:37 +0800

    websocket相关测试

--- MyWpfDemo/WpfApp1/ViewModel/MainViewModel.cs
+++ MyWpfDemo/WpfApp1/ViewModel/MainViewModel.cs
@@ -1,11 +1,13 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
 
 namespace WpfApp1.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
     /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// Use the <strong> mvvminpc </strong> snippet to add bindable properties to this ViewModel.
     /// </para>
     /// <para>
     /// You can also use Blend to data bind with the tool's support.
@@ -16,19 +18,54 @@ namespace WpfApp1.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        //BUG
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            OnButtonCommand = new RelayCommand(ButtonCommand);
+            AddStringToList = new RelayCommand(AddString);
+        }
+
+        public RelayCommand AddStringToList { get; set; }
+
+        public List<string> ListSource
+        {
+            get { return M_ListSource; }
+            set { Set(ref M_ListSource, value); }
+        }
+
+        public int Num { get; set; } = 0;
+
+        /// <summary>
+        /// button click ´¦Àí
+        /// </summary>
+        public RelayCommand OnButtonCommand { get; set; }
+
+        public string TextStr
+        {
+            get { return sTextStr; }
+            set { Set(ref sTextStr, value); }
+        }
+
+        private List<string> M_ListSource = new List<string>();
+
+        private string sTextStr = "444444444444";
+
+        public void Test()
+        {
+        }
+
+        private void ButtonCommand()
+        {
+            TextStr = $"ButtonCommand + {Num++}";
+        }
+
+        private void AddString()
+        {
+            ListSource.Add("test ¿×ÏéÀ×");
+        }
+
+        private void test32()
+        {
         }
     }
 }
---
+++ MyWpfDemo/websocketClient/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace websocketClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public string TextStr { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MywebSocket = new ClientWebSocket();
+        }
+
+        private void SendReq_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "ws://192.168.0.102:60020/test/crte";
+
+            if (MywebSocket == null)
+            {
+                MywebSocket = new ClientWebSocket();
+                TextStr.Text = "new suc";
+            }
+
+            if (MywebSocket.State != WebSocketState.Open)
+            {
+                MywebSocket.ConnectAsync(new Uri(url), new System.Threading.CancellationToken());
+                TextStr.Text = "ConnectAsync suc";
+            }
+        }
+
+        public ClientWebSocket MywebSocket { get; set; }
+
+        private void SendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            var bSend = new byte[32]; // = { 0x01, 0x02};
+            for (int i = 0; i < 32; i++)
+            {
+                bSend[i] = 0x31;
+            }
+
+            if (MywebSocket.State == WebSocketState.Open)
+            {
+                MywebSocket.SendAsync(new ArraySegment<byte>(bSend), WebSocketMessageType.Text, true, new System.Threading.CancellationToken());
+            }
+            else
+            {
+                TextStr.Text = "send msg failed";
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            if (MywebSocket.State == WebSocketState.Open)
+            {
+                MywebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "close", new System.Threading.CancellationToken()); ;
+                MywebSocket.Dispose();
+                TextStr.Text = MywebSocket.State.ToString();
+                MywebSocket = null;
+            }
+            else
+            {
+                TextStr.Text = "open failed";
+            }
+        }
+    }
+}
