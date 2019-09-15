94e052f8e8443207605bba4186ac5094642cf61d
fsrd017 <277892799@qq.com>
2019-09-15 12:10:00 +0800

    支持动态分割

--- MyWpfDemo/MainWindows/Window1.xaml.cs
+++ MyWpfDemo/MainWindows/Window1.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MainWindows
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
-
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    listBox.Width = 0;
-        //}
-
-        //private void Button_Click_1(object sender, RoutedEventArgs e)
-        //{
-        //    listBox.Width = 140;
-        //}
     }
 }
