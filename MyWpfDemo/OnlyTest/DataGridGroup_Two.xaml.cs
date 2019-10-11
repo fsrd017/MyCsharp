4891fedd98f8510933f8b49f328e3af57b4f3faa
fsrd017 <277892799@qq.com>
2019-10-11 22:10:19 +0800

    1. 提交 treelistVewe 2. datagrid修改

---
+++ MyWpfDemo/OnlyTest/DataGridGroup_Two.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnlyTest {
+
+    /// <summary>
+    /// DataGridGroup.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataGridGroupTwo : Window {
+
+        private ObservableCollection<Model> data = new ObservableCollection<Model>();
+
+        public DataGridGroupTwo()
+        {
+            InitializeComponent();
+
+            dataGrid1.ItemsSource = data;
+            for (int i = 0; i < 13; i++) {
+                Model m = CreateModel("张" + i, i / 4 + "年级", (i % 2 == 0 ? "女" : "男"));
+                data.Add(m);
+            }
+            ICollectionView vw = CollectionViewSource.GetDefaultView(data);
+            vw.GroupDescriptions.Add(new PropertyGroupDescription("Sex"));
+            vw.GroupDescriptions.Add(new PropertyGroupDescription("Category"));
+        }
+
+        private Model CreateModel(string name, string cate, string sex)
+        {
+            Model model = new Model();
+            model.Name = name;
+            model.Category = cate;
+            model.Sex = sex;
+            return model;
+        }
+    }
+
+}
---
+++ MyWpfDemo/TreeListViewSample/TreeListViewSample/Controls/MyStyleSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TreeListViewSample.Controls
+{
+    public class MyStyleSelector : StyleSelector
+    {
+        public Style Style1 { get; set; }
+        public Style Style2 { get; set; }
+
+        private int internalCounter = 1;
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            Style stl;
+            stl = internalCounter == 1 ? Style1 : Style2;
+
+            if (++internalCounter > 2)
+            {
+                internalCounter = 1;
+            }
+
+            return stl;
+        }
+    }
+}
